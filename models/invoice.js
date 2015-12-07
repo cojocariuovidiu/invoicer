3b899b9bd92d45aebec6a5fd5669ceeb1851b19f
marb61a <marb61a@gmail.com>
2015-12-07 09:04:44 +0000

    added invoice model

---
+++ models/invoice.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+// Invoice Schema
+var invoiceSchema = mongoose.Schema({
+	customer:{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Customer'
+	},
+	service:{
+		type: String,
+		required: true
+	},
+	price:{
+		type: String
+	},
+	due:{
+		type: String
+	},
+	status:{
+		type: String
+	},
+	createdAt:{
+		type: Date,
+		default: Date.now
+	},
+});
+
+var Invoice = module.exports = mongoose.model('Invoice', invoiceSchema);
