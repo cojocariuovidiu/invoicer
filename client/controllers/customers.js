adbb30ef6135bb838a50d97f46927159574dcca2
marb61a <marb61a@gmail.com>
2015-12-22 13:54:06 +0000

    updated customer controller

--- client/controllers/customers.js
+++ client/controllers/customers.js
@@ -9,6 +9,20 @@ myApp.controller("CustomersController", ['$scope', '$http','$location','$routePa
 		});
 	}
   
+  $scope.getCustomer = function(){
+		var id = $routeParams.id;
+		$http.get('/api/customers/'+id).success(function(response){
+			$scope.customer = response;
+		});
+	}
+  
+  $scope.getCustomerInvoices = function(){
+		var id = $routeParams.id;
+		$http.get('/api/invoices/customer/'+id).success(function(response){
+			$scope.customer_invoices = response;
+		});
+	}
+  
   $scope.addCustomer = function(){
 		$http.post('/api/customers/',$scope.customer).success(function(response){
 			window.location.href='/#customers';
@@ -20,4 +34,11 @@ myApp.controller("CustomersController", ['$scope', '$http','$location','$routePa
 			window.location.href='/#customers';
 		});
 	}
-}])
+  
+  $scope.deleteCustomer = function(id){
+		$http.delete('/api/customers/'+id).success(function(response){
+			window.location.href='/#customers';
+		});
+	}
+  
+}]);
